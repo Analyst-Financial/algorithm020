cdf403b46a01f08f6581592d6cd8295a3f7c5105
Analyst-Financial <xinbeitewangling@163.com>
2020-12-21 00:47:51 +0800

    'addweek6'

---
+++ Week6/Week6.java
@@ -0,0 +1,135 @@
+package algorithm;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName Week6
+ * @Description
+ * @Author Administrator
+ * @Date 2020/12/20  21:30
+ * @Version 1.0
+ **/
+public class Week6 {
+
+    /**
+     * 62. 不同路径
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+     * 问总共有多少条不同的路径？
+     */
+
+    public int uniquePaths(int m, int n) {
+        int[][] f = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            f[i][0] = 1;
+        }
+        for (int j = 0; j < n; ++j) {
+            f[0][j] = 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+
+    /**
+     * 超哥方法1
+     */
+    public int uniquePaths2(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; ++i) dp[i][0] = 1;
+        for (int j = 0; j < n; ++j) dp[0][j] = 1;
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+     * 超哥方法2
+     */
+    public int uniquePaths3(int m, int n) {
+        int[] cur = new int[n];
+        Arrays.fill(cur, 1);
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
+    }
+
+    /**
+     * 63. 不同路径 II
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+     * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+     * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+     * 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+     * 输出：2
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int width = obstacleGrid[0].length;
+        int[] dp = new int[width];
+        dp[0] = 1;
+        for (int[] row : obstacleGrid) {
+            for (int j = 0; j < width; j++) {
+                if (row[j] == 1)
+                    dp[j] = 0;
+                else if (j > 0)
+                    dp[j] += dp[j - 1];
+            }
+        }
+        return dp[width - 1];
+    }
+
+    /**
+     * 1143. 最长公共子序列
+     * 给定两个字符串text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+     * 一个字符串的子序列是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+     * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+     * 若这两个字符串没有公共子序列，则返回 0。
+     * 输入：text1 = "abcde", text2 = "ace"
+     * 输出：3
+     * https://leetcode.com/problems/longest-common-subsequence/discuss/351689/JavaPython-3-Two-DP-codes-of-O(mn)-and-O(min(m-n))-spaces-w-picture-and-analysis
+     */
+    public int longestCommonSubsequence(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i < s1.length(); ++i)
+            for (int j = 0; j < s2.length(); ++j)
+                if (s1.charAt(i) == s2.charAt(j)) dp[i + 1][j + 1] = 1 + dp[i][j];
+                else dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+        return dp[s1.length()][s2.length()];
+    }
+
+    public int longestCommonSubsequence2(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        if (m < n) return longestCommonSubsequence(s2, s1);
+        int[][] dp = new int[2][n + 1];
+        for (int i = 0, k = 1; i < m; ++i, k ^= 1)
+            for (int j = 0; j < n; ++j)
+                if (s1.charAt(i) == s2.charAt(j)) dp[k][j + 1] = 1 + dp[k ^ 1][j];
+                else dp[k][j + 1] = Math.max(dp[k ^ 1][j + 1], dp[k][j]);
+        return dp[m % 2][n];
+    }
+
+    public int longestCommonSubsequence3(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        if (m < n) {
+            return longestCommonSubsequence(text2, text1);
+        }
+        int[] dp = new int[n + 1];
+        for (int i = 0; i < text1.length(); ++i) {
+            for (int j = 0, prevRow = 0, prevRowPrevCol = 0; j < text2.length(); ++j) {
+                prevRowPrevCol = prevRow;
+                prevRow = dp[j + 1];
+                dp[j + 1] = text1.charAt(i) == text2.charAt(j) ? prevRowPrevCol + 1 : Math.max(dp[j], prevRow);
+            }
+        }
+        return dp[n];
+    }
+}
